a4d92ed425b001ac736b55b7dd024e20e4a610ce
Neelansh <sethineelansh@gmail.com>
2026-07-20 03:56:28 +0530

    Add Swift Package Manager support

--- ios/ui_background_task.podspec
+++ ios/ui_background_task.podspec
@@ -13,7 +13,7 @@ A new Flutter project.
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Your Company' => 'email@example.com' }
   s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
+  s.source_files = 'ui_background_task/Sources/ui_background_task/**/*.swift'
   s.dependency 'Flutter'
   s.platform = :ios, '9.0'
 
--- ios/ui_background_task/Sources/ui_background_task/UiBackgroundTaskPlugin.swift
+++ ios/ui_background_task/Sources/ui_background_task/UiBackgroundTaskPlugin.swift
@@ -1,48 +1,47 @@
 import Flutter
 import UIKit
 
-public class SwiftUiBackgroundTaskPlugin: NSObject, FlutterPlugin {
-    
-    
-    var runningTaskDictionary = Dictionary<UIBackgroundTaskIdentifier, BackgroundTask>()
+public class UiBackgroundTaskPlugin: NSObject, FlutterPlugin {
+    var runningTaskDictionary = [UIBackgroundTaskIdentifier: BackgroundTask]()
+
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "ui_background_task", binaryMessenger: registrar.messenger())
-        let instance = SwiftUiBackgroundTaskPlugin()
+        let channel = FlutterMethodChannel(
+            name: "ui_background_task",
+            binaryMessenger: registrar.messenger()
+        )
+        let instance = UiBackgroundTaskPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        
         switch call.method {
-            
         case "beginBackgroundTask":
             let application = UIApplication.shared
             BackgroundTask.run(application: application) { backgroundTask in
                 runningTaskDictionary[backgroundTask.identifier] = backgroundTask
                 result(backgroundTask.identifier.rawValue)
             }
-            break;
-            
+
         case "endBackgroundTask":
-            
             let args = call.arguments as? [String: Any?]
             let taskId = args?["taskId"] as? Int
-            
-            if taskId == nil {
+
+            guard let taskId else {
                 result(nil)
                 return
             }
-            
-            let backgroundTask: BackgroundTask? = runningTaskDictionary[UIBackgroundTaskIdentifier(rawValue: taskId!)]
-            
-            if backgroundTask != nil {
-                backgroundTask?.end();
+
+            let identifier = UIBackgroundTaskIdentifier(rawValue: taskId)
+            if let backgroundTask = runningTaskDictionary[identifier] {
+                backgroundTask.end()
                 result(nil)
             }
-            break;
-            
+
         default:
             result(FlutterMethodNotImplemented)
         }
     }
 }
+
+@available(*, deprecated, renamed: "UiBackgroundTaskPlugin")
+public typealias SwiftUiBackgroundTaskPlugin = UiBackgroundTaskPlugin
